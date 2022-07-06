5f8370ec146ba2308318535cd557ea58edcd1111
Kevin Blin <kblin@outlook.com>
2022-07-06 12:45:30 +0200

    add euler instructions

---
+++ euler_setup.sh
@@ -0,0 +1,25 @@
+# Usage: sh euler_setup.sh <username> <sweepID>
+
+cd ..
+
+cd ..
+
+cd scratch/$1
+
+env2lmod
+
+module load gcc/8.2.0
+
+module load python_gpu/3.8.5
+
+module load eth_proxy
+
+python -m venv venv 
+
+source venv/bin/activate 
+
+pip install -r requirements.txt
+
+wandb login 
+
+bsub -W 23:00 -R "rusage[mem=64000, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=10240]" wandb agent --count 25 $2
